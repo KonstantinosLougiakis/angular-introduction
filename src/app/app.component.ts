322a94bb59d9f3670bc8816e96baa4a4c55eda23
KonstantinosLougiakis <lougiokostas@gmail.com>
2024-03-30 07:42:26 +0200

    component input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,20 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 })
 export class AppComponent {
   name = 'Konstantinos';
+
+  person0 = {
+    givenName: 'Konstantinos',
+    surName: 'Lougiakis',
+    age: 27,
+    email: 'lougiokostas@aueb.gr',
+    address: 'Athens, Greece'
+  };
+
+  person1 = {
+    givenName: 'John',
+    surName: 'Doe',
+    age: 62,
+    email: 'john@aueb.gr',
+    address: 'New York, USA'
+  };
 }
